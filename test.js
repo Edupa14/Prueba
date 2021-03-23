4fbf7be241d8d1c358e0d0f5ca3f1d97e391600c
Edupa14 <33420774+Edupa14@users.noreply.github.com>
2021-03-23 12:40:49 -0500

    Update and rename ext.js to test.js

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
+
+(function () {
+    
 console.log("test");
 var socket = io.connect('http://192.168.0.12:6677', {'forceNew':true})
 message = "hola estoy en linea"
 socket.emit('consola', message)
+   
+})();
