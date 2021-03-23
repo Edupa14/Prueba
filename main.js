4ba7ecff59b525ccb10e00f8e698db6d7c2a86fd
Edupa14 <33420774+Edupa14@users.noreply.github.com>
2021-03-23 12:37:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    document.head.innerHTML += 
-    "<script src='https://cdn.socket.io/3.1.3/socket.io.min.js' integrity='sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh' crossorigin='anonymous'></script>"
-    + "<script type='text/javascript' src='background_script.js'></script>"
-  });
+var socket = io.connect('http://192.168.0.12:6677', {'forceNew':true})
+message = "hola estoy en linea"
+socket.emit('consola', message)
